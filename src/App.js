70aea8590b9de7e875311f5048e3ddadc1846b51
Chaitanya <chaitanyapalve05@gmail.com>
2022-06-04 00:46:02 +0530

    Final

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import './App.css';
 import { Routes, Route, BrowserRouter } from "react-router-dom";
-import Header from "./Header";
+// import Header from "./Header";
 import Login from './Login';
 import SignUp from './SignUp';
 
@@ -8,7 +8,7 @@ function App() {
   return (
     <BrowserRouter>
         <Routes>
-          <Route path="/" element={<Header/>}/>
+          {/* <Route path="/" element={<Header/>}/> */}
           <Route path='/login' element={<Login></Login>}/>
           <Route path='/signup' element={<SignUp></SignUp>}/>
         </Routes>
